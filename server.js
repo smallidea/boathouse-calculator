01eaa0e51ba631ba82cac9ca466e04593b5d4963
Lei Xu <leixu@leansoftx.com>
2022-05-27 12:51:03 +0000

    smoke

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
+console.log("SmartTEST CLI k8s mode");
 /**
  * Normalize a port into a number, string, or false.
  */
