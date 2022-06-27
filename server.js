003ed5a55c51a45b8e148a7c3c2db45a110b2b18
Lei Xu <leixu@leansoftx.com>
2022-06-27 02:31:18 +0000

    smoke 2022.6.27 cli local/remote/k8s

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
-console.log("smoke 2022.6.27 cli local/remote");
+console.log("smoke 2022.6.27 cli local/remote/k8s");
 /**
  * Normalize a port into a number, string, or false.
  */
