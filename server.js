47c0734f36aa83db3527a4c6430ec0d1abb6c6e1
leebchi <chileeb@163.com>
2022-08-23 00:01:05 +0000

    SmartIDE MacOs Smoke0823

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-/** SmartIDE MacOs Smoke0822:Local Host/Remote/K8S;Server Host/K8S√ */
+/** SmartIDE MacOs Smoke0823:
+ * Local  Host√/Remote/K8S;
+ * Server Host/K8S */
+ 
 /**
  * Module dependencies. 
  */
